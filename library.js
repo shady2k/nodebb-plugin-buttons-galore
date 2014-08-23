8812e3ccfc7aa5117d5fa7bf40f4aeba57c3a71f
TedRinehart <theodorerinehart@gmail.com>
2014-08-23 13:04:00 -0500

    Update library.js
    We're replacing the code class, not spoilers in this version.

--- library.js
+++ library.js
@@ -5,7 +5,7 @@ var plugin = {};
 plugin.parse = function(postContent, callback) {
 	// this regex could be better
 	postContent = postContent
-		.replace(/<p>! *([\S\s]*?)<\/p>/gm, '</blockquote><blockquote class="spoiler"><p>$1</p></blockquote><blockquote>')
+		.replace(/<p>! *([\S\s]*?)<\/p>/gm, '</blockquote><blockquote class="code"><p>$1</p></blockquote><blockquote>')
 		.replace(/<blockquote>\s*<\/blockquote>/g, '');
 
 	callback(null, postContent);
