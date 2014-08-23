6f893d2086a7d4bd8b2fe130ce941dc4f1186b66
TedRinehart <theodorerinehart@gmail.com>
2014-08-23 12:49:49 -0500

    Update and rename spoilers.js to buttons.js
    Not really sure how insert multiple lines in Markdown - going to play with this and give it a go.
    I'm also being lazy and not using the spoiler icon for the time being.

--- static/buttons.js
+++ static/buttons.js
@@ -2,10 +2,10 @@ $('document').ready(function() {
 	require(['composer', 'composer/controls'], function(composer, controls) {
 		composer.addButton('fa fa-eye-slash', function(textarea, selectionStart, selectionEnd) {
 			if(selectionStart === selectionEnd){
-				controls.insertIntoTextarea(textarea, ">! Spoiler");
-				controls.updateTextareaSelection(textarea, selectionStart + 3, selectionStart + 12);
+				controls.insertIntoTextarea(textarea, "```",'"Insert Code Here"',"```");
+				controls.updateTextareaSelection(textarea, selectionStart + 3, selectionStart + 26);
 			} else {
-				controls.wrapSelectionInTextareaWith(textarea, '>! ', '');
+				controls.wrapSelectionInTextareaWith(textarea, '```', '```');
 				controls.updateTextareaSelection(textarea, selectionStart + 3, selectionEnd + 3);
 			}
 		});
