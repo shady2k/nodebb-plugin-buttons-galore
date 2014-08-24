a0fd6b6f4a4732c326335b3d0cc196179e06e4d1
TedRinehart <theodorerinehart@gmail.com>
2014-08-24 17:17:57 -0500

    Update buttons.js
    /n should escape and throw new lines. If this works I'm a bloody idiot.

--- static/buttons.js
+++ static/buttons.js
@@ -2,12 +2,10 @@ $('document').ready(function() {
 	require(['composer', 'composer/controls'], function(composer, controls) {
 		composer.addButton('fa fa-eye-slash', function(textarea, selectionStart, selectionEnd) {
 			if(selectionStart === selectionEnd){
-				controls.insertIntoTextarea(textarea, '```',
-				'',
-				'```');
+				controls.insertIntoTextarea(textarea, '```'/n"Insert Language Name"/n'```');
 				controls.updateTextareaSelection(textarea, selectionStart + 3, selectionStart + 26);
 			} else {
-				controls.wrapSelectionInTextareaWith(textarea, '```', '```');
+				controls.wrapSelectionInTextareaWith(textarea, '```','```');
 				controls.updateTextareaSelection(textarea, selectionStart + 3, selectionEnd + 3);
 			}
 		});
