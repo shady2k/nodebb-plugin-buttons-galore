78b12944858d0b6b7eb85b705771faf265775b9b
TedRinehart <theodorerinehart@gmail.com>
2014-08-24 17:22:51 -0500

    Update buttons.js
    Bloody hell, I already made that mistake

--- static/buttons.js
+++ static/buttons.js
@@ -2,10 +2,10 @@ $('document').ready(function() {
 	require(['composer', 'composer/controls'], function(composer, controls) {
 		composer.addButton('fa fa-eye-slash', function(textarea, selectionStart, selectionEnd) {
 			if(selectionStart === selectionEnd){
-				controls.insertIntoTextarea(textarea, '```'/n"Insert Language Name"/n'```');
+				controls.insertIntoTextarea(textarea, '```/n',"Insert Language Name",'/n```');
 				controls.updateTextareaSelection(textarea, selectionStart + 3, selectionStart + 26);
 			} else {
-				controls.wrapSelectionInTextareaWith(textarea, '```','```');
+				controls.wrapSelectionInTextareaWith(textarea, '```/n','/n```');
 				controls.updateTextareaSelection(textarea, selectionStart + 3, selectionEnd + 3);
 			}
 		});
