2768955c8f4d54524a94334b72b9b04c1bf655b2
TedRinehart <theodorerinehart@gmail.com>
2014-08-24 16:47:18 -0500

    Update buttons.js
    That was a bad idea. NodeBB wouldn't start. Need commas.

--- static/buttons.js
+++ static/buttons.js
@@ -2,9 +2,9 @@ $('document').ready(function() {
 	require(['composer', 'composer/controls'], function(composer, controls) {
 		composer.addButton('fa fa-eye-slash', function(textarea, selectionStart, selectionEnd) {
 			if(selectionStart === selectionEnd){
-				controls.insertIntoTextarea(textarea, "```"
-				"Insert Code Here"
-				"```");
+				controls.insertIntoTextarea(textarea, '```',
+				'',
+				'```');
 				controls.updateTextareaSelection(textarea, selectionStart + 3, selectionStart + 26);
 			} else {
 				controls.wrapSelectionInTextareaWith(textarea, '```', '```');
