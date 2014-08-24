a8e865162772af918cf3543362709dfff2b7bf0a
TedRinehart <theodorerinehart@gmail.com>
2014-08-24 17:38:54 -0500

    Update buttons.js
    That actually worked.
    
    Changed insert message and icon.

--- static/buttons.js
+++ static/buttons.js
@@ -1,8 +1,8 @@
 $('document').ready(function() {
 	require(['composer', 'composer/controls'], function(composer, controls) {
-		composer.addButton('fa fa-eye-slash', function(textarea, selectionStart, selectionEnd) {
+		composer.addButton('fa fa-slack', function(textarea, selectionStart, selectionEnd) {
 			if(selectionStart === selectionEnd){
-				controls.insertIntoTextarea(textarea, '```\n"Insert Language Name"\n```');
+				controls.insertIntoTextarea(textarea, '```\nInsert Code Here\n```');
 				controls.updateTextareaSelection(textarea, selectionStart + 3, selectionStart + 26);
 			} else {
 				controls.wrapSelectionInTextareaWith(textarea, '```\n','\n```');
