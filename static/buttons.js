f223e23104ba45f1a691865035ec7f6b8d4d9d3d
TedRinehart <theodorerinehart@gmail.com>
2014-08-24 19:00:59 -0500

    Update buttons.js
    Testing updating the selected area

--- static/buttons.js
+++ static/buttons.js
@@ -3,19 +3,19 @@ $('document').ready(function() {
 		composer.addButton('fa fa-code', function(textarea, selectionStart, selectionEnd) {
 			if(selectionStart === selectionEnd){
 				controls.insertIntoTextarea(textarea, '```\nInsert Code Here\n```');
-				controls.updateTextareaSelection(textarea, selectionStart + 3, selectionStart + 26);
+				controls.updateTextareaSelection(textarea, selectionStart + 4);
 			} else {
 				controls.wrapSelectionInTextareaWith(textarea, '```\n','\n```');
-				controls.updateTextareaSelection(textarea, selectionStart + 3, selectionEnd + 3);
+				controls.updateTextareaSelection(textarea, selectionStart + 4, selectionEnd);
 			}
 		});
 		composer.addButton('fa fa-strikethrough', function(textarea, selectionStart, selectionEnd) {
 			if(selectionStart === selectionEnd){
 				controls.insertIntoTextarea(textarea, '~~Insert Text Here~~');
-				controls.updateTextareaSelection(textarea, selectionStart + 3, selectionStart + 26);
+				controls.updateTextareaSelection(textarea, selectionStart + 2);
 			} else {
 				controls.wrapSelectionInTextareaWith(textarea, '~~','~~');
-				controls.updateTextareaSelection(textarea, selectionStart + 3, selectionEnd + 3);
+				controls.updateTextareaSelection(textarea, selectionStart + 2, selectionEnd);
 			}
 		});
 	});
