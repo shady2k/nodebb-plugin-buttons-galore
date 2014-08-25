85ee6267c3836cec3a49dd61ff08ead78c939afb
TedRinehart <theodorerinehart@gmail.com>
2014-08-24 20:19:00 -0500

    Update buttons.js
    Will need to fix selections for the quote button. Testing to see what happens.

--- buttons.js
+++ buttons.js
@@ -1,5 +1,14 @@
 $('document').ready(function() {
 	require(['composer', 'composer/controls'], function(composer, controls) {
+		composer.addButton('fa fa-strikethrough', function(textarea, selectionStart, selectionEnd) {
+			if(selectionStart === selectionEnd){
+				controls.insertIntoTextarea(textarea, '~~Insert Text Here~~');
+				controls.updateTextareaSelection(textarea, selectionStart + 2, selectionEnd + 18);
+			} else {
+				controls.wrapSelectionInTextareaWith(textarea, '~~','~~');
+				controls.updateTextareaSelection(textarea, selectionStart + 2, selectionEnd + 2);
+			}
+		});		
 		composer.addButton('fa fa-code', function(textarea, selectionStart, selectionEnd) {
 			if(selectionStart === selectionEnd){
 				controls.insertIntoTextarea(textarea, '```\nInsert Code Here\n```');
@@ -9,14 +18,15 @@ $('document').ready(function() {
 				controls.updateTextareaSelection(textarea, selectionStart + 4, selectionEnd + 4);
 			}
 		});
-		composer.addButton('fa fa-strikethrough', function(textarea, selectionStart, selectionEnd) {
+		composer.addButton('fa fa-quote-right', function(textarea, selectionStart, selectionEnd) {
 			if(selectionStart === selectionEnd){
-				controls.insertIntoTextarea(textarea, '~~Insert Text Here~~');
-				controls.updateTextareaSelection(textarea, selectionStart + 2, selectionEnd + 18);
+				controls.insertIntoTextarea(textarea, '@Username / Website said:\n> \n\n\n');
+				controls.updateTextareaSelection(textarea, selectionStart + 4, selectionEnd + 31);
 			} else {
-				controls.wrapSelectionInTextareaWith(textarea, '~~','~~');
-				controls.updateTextareaSelection(textarea, selectionStart + 2, selectionEnd + 2);
+				controls.wrapSelectionInTextareaWith(textarea, '@Username / Website said:\n> \n\n\n');
+				controls.updateTextareaSelection(textarea, selectionStart + 4, selectionEnd + 4);
 			}
-		});
+		});		
+
 	});
 });
