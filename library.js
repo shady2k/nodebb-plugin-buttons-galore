707efe02232f675afcae07274154027fd11bb73e
Dias Aurélien <aurelien.esiao@gmail.com>
2014-10-26 23:38:35 +0100

    Only one was modified because of the starting

--- library.js
+++ library.js
@@ -7,18 +7,19 @@ plugin.parse = function(postContent, callback) {
 	//console.log(postContent);
 
 	postContent = postContent
+
 			//Handle line breaks inside a paragraph.
 			.replace(/\n/g, "<br>")
 			//Text align left
-			.replace(/[^`]?^<p>&lt;-((?:.|\n)*?)&lt;-<\/p>/gm,'<p class="text-left">$1</p>')
+			.replace(/[^`]?<p>&lt;-((?:.|\n)*?)&lt;-<\/p>/gm,'<p class="text-left">$1</p>')
 			//Text align center
-			.replace(/[^`]?^<p>-&gt;((?:.|\n)*?)&lt;-<\/p>/gm,'<p class="text-center">$1</p>')
+			.replace(/[^`\n]?<p>-&gt;((?:.|\n)*?)&lt;-<\/p>/gm,'<p class="text-center">$1</p>')
 			//Text align right
-			.replace(/[^`]?^<p>-&gt;((?:.|\n)*?)-&gt;<\/p>/gm,'<p class="text-right">$1</p>')
+			.replace(/[^`]?<p>-&gt;((?:.|\n)*?)-&gt;<\/p>/gm,'<p class="text-right">$1</p>')
 			//Text align center
-			.replace(/[^`]?^<p>=&gt;((?:.|\n)*?)&lt;=<\/p>/gm,'<p class="text-justify">$1</p>')
+			.replace(/[^`]?<p>=&gt;((?:.|\n)*?)&lt;=<\/p>/gm,'<p class="text-justify">$1</p>')
 			//Underlined text.
-			.replace(/[^`]?^~((?:.|\n)*?)~/g, "<u>$1</u>");
+			.replace(/[^`]?~((?:.|\n)*?)~/g, "<u>$1</u>");
 
 	callback(null, postContent);
 };
